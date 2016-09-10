fe441bdc392bcd41c96622887cf3f6195acae5b1
1211120248 <1211120248@qq.com>
2016-09-10 18:30:45 +0900

    我是开发者 我是用户页面

--- 知码网/js/index.js
+++ 知码网/js/index.js
@@ -1,3 +1,17 @@
 $(function(){
     new WOW().init();
+    //流程插件
+    var stepBar = getStepBar();
+    stepBar.init("stepBar", {
+        step : 5,
+        change : true,
+        animation : true
+    });
+
+    var stepBar2 = getStepBar();
+    stepBar2.init("stepBar2", {
+        step : 5,
+        change : true,
+        animation : true
+    });
 });
